6750ada51732a96b44e5a0f05ab616b90b32fad6
Dylan-Cmt <cormet.dylan@gmail.com>
2026-04-08 10:54:43 +0200

    remove unnecessary comments

--- visualizers.py
+++ visualizers.py
@@ -7,7 +7,7 @@ def visualize_point(point, tk):
 # Visualize the generated cloud points
 def visualize_points(points):
     x, y = zip(*points)  # Unzip the list of points into x and y coordinates
-    plt.scatter(x, y, color='black', alpha=0.5, s=5) # Scatter plot of the points
+    plt.scatter(x, y, color='black', alpha=0.5, s=5)
     # title is set in main.py
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
@@ -18,9 +18,9 @@ def visualize_points(points):
 # Visualize 2 sets of points (e.g., cloud points and curve points)
 def visualize_two_sets_of_points(points1, points2):
     x1, y1 = zip(*points1)  # Unzip the first set of points into x and y coordinates
-    x2, y2 = zip(*points2)  # Unzip the second set of points into x and y coordinates
-    plt.scatter(x1, y1, color='black', alpha=0.5, s=5, label='Cloud Points') # Scatter plot of the first set of points
-    plt.scatter(x2, y2, color='red', alpha=0.5, s=5, label='Curve Points') # Scatter plot of the second set of points
+    x2, y2 = zip(*points2)
+    plt.scatter(x1, y1, color='black', alpha=0.5, s=5, label='Cloud Points')
+    plt.scatter(x2, y2, color='red', alpha=0.5, s=5, label='Curve Points')
     # title is set in main.py
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
