533bac97c60c11fbaccd5f65428d017ac6e3fb8a
Dylan-Cmt <cormet.dylan@gmail.com>
2026-04-20 14:34:34 +0200

    update functions (adding knots/degree informations where it needs)

--- src/bspline_curves.py
+++ src/bspline_curves.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def bspline_basis(i, degree, t):
+def bspline_basis(i, degree, t, knots):
     n = len(knots) - 1
     if degree == 0:
         if i >= n:
@@ -11,34 +11,34 @@ def bspline_basis(i, degree, t):
     if (i + degree) < n:
         denom1 = knots[i + degree] - knots[i]
         if denom1 != 0:
-            first_part = (t - knots[i]) / denom1 * bspline_basis(i, degree - 1, t)
+            first_part = (t - knots[i]) / denom1 * bspline_basis(i, degree - 1, t, knots)
     if (i + degree + 1) < n:
         denom2 = knots[i + degree + 1] - knots[i + 1]
         if denom2 != 0:
             second_part = (
                 (knots[i + degree + 1] - t)
                 / denom2
-                * bspline_basis(i + 1, degree - 1, t)
+                * bspline_basis(i + 1, degree - 1, t, knots)
             )
     return first_part + second_part
  
-def dt_bspline_basis(i, degree, t): # cf p59 du NURBS BOOK
+def dt_bspline_basis(i, degree, t, knots): # cf p59 du NURBS BOOK
     first_part, second_part = 0.0, 0.0
     denom1 = knots[i + degree] - knots[i]
     if denom1 != 0:
-        first_part  = degree / denom1 * bspline_basis(i, degree - 1, t)
+        first_part  = degree / denom1 * bspline_basis(i, degree - 1, t, knots)
     denom2 = knots[i + degree + 1] - knots[i + 1]
     if denom2 != 0:
-        second_part = degree / denom2 * bspline_basis(i + 1, degree - 1, t)
+        second_part = degree / denom2 * bspline_basis(i + 1, degree - 1, t, knots)
     return first_part - second_part
 
-def dtt_bspline_basis(i, degree, t): # cf p62 du NURBS BOOK
+def dtt_bspline_basis(i, degree, t, knots): # cf p62 du NURBS BOOK
     first_part, second_part, third_part = 0.0, 0.0, 0.0
 
     # N_{i, p-2}
     denom1 = (knots[i + degree] - knots[i]) * (knots[i + degree - 1] - knots[i])
     if denom1 != 0:
-        first_part = degree * (degree - 1) / denom1 * bspline_basis(i, degree - 2, t)
+        first_part = degree * (degree - 1) / denom1 * bspline_basis(i, degree - 2, t, knots)
 
     # N_{i+1, p-2}
     denom2a = (knots[i + degree] - knots[i]) * (knots[i + degree] - knots[i + 1])
@@ -50,39 +50,40 @@ def dtt_bspline_basis(i, degree, t): # cf p62 du NURBS BOOK
     if denom2b != 0:
         coeff2 += 1.0 / denom2b
 
-    second_part = degree * (degree - 1) * coeff2 * bspline_basis(i + 1, degree - 2, t)
+    second_part = degree * (degree - 1) * coeff2 * bspline_basis(i + 1, degree - 2, t, knots)
 
     # N_{i+2, p-2}
     denom3 = (knots[i + degree + 1] - knots[i + 1]) * (knots[i + degree + 1] - knots[i + 2])
     if denom3 != 0:
-        third_part = degree * (degree - 1) / denom3 * bspline_basis(i + 2, degree - 2, t)
+        third_part = degree * (degree - 1) / denom3 * bspline_basis(i + 2, degree - 2, t, knots)
 
     return first_part - second_part + third_part
 
  
-def bspline_curve(P, t):
+def bspline_curve(P, t, knots, degree):
     t_min = knots[degree]
     t_max = knots[-degree - 1]
     curve = np.zeros(P.shape[1])
     for i in range(len(P)):
-        N = bspline_basis(i, degree, t)
-        curve += N * P[i]
-    return curve
- 
-def dt_bspline_curve(P, t):
-    t_min = knots[degree]
-    t_max = knots[-degree - 1]
-    curve = np.zeros(P.shape[1])
-    for i in range(len(P)):
-        N = dt_bspline_basis(i, degree, t)
+        N = bspline_basis(i, degree, t, knots)
         curve += N * P[i]
     return curve
 
-def dtt_bspline_curve(P, t):
+ 
+def dt_bspline_curve(P, t, knots, degree):
     t_min = knots[degree]
     t_max = knots[-degree - 1]
     curve = np.zeros(P.shape[1])
     for i in range(len(P)):
-        N = dtt_bspline_basis(i, degree, t)
+        N = dt_bspline_basis(i, degree, t, knots)
+        curve += N * P[i]
+    return curve
+
+def dtt_bspline_curve(P, t, knots, degree):
+    t_min = knots[degree]
+    t_max = knots[-degree - 1]
+    curve = np.zeros(P.shape[1])
+    for i in range(len(P)):
+        N = dtt_bspline_basis(i, degree, t, knots)
         curve += N * P[i]
     return curve
