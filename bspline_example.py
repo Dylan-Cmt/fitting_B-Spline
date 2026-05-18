4a3d4767ccea33cb91f23caaf4faef3b0e45b09f
Dylan-Cmt <cormet.dylan@gmail.com>
2026-05-18 12:19:53 +0200

    update examples with best degree and best control points

--- bspline_example.py
+++ bspline_example.py
@@ -6,24 +6,24 @@ from src.cloud_points import *
 from src.visualizers import *
 import time
 
-def compare3methods(P, T, X, max_iter=100):
+def compare3methods(P, T, X, max_iter=100, constraint=True):
     # PDM
     start_time_PD = time.time()
-    optimized_control_points_PD, tot_iter_PD, avg_error_PD = gradient_descent_PD(P, T, knots, degree, X, max_iter=max_iter, constraint=True)
+    optimized_control_points_PD, tot_iter_PD, avg_error_PD = gradient_descent_PD(P, T, knots, degree, X, max_iter=max_iter, constraint=constraint)
     end_time_PD = time.time()
     tot_time_PD = end_time_PD - start_time_PD
     print(f"Optimization time for PDM: {tot_time_PD:.2f} seconds")
 
     # TDM
     start_time_TD = time.time()
-    optimized_control_points_TD, tot_iter_TD, avg_error_TD = gradient_descent_TD(P, T, knots, degree, X, max_iter=max_iter, constraint=True)
+    optimized_control_points_TD, tot_iter_TD, avg_error_TD = gradient_descent_TD(P, T, knots, degree, X, max_iter=max_iter, constraint=constraint)
     end_time_TD = time.time()
     tot_time_TD = end_time_TD - start_time_TD
     print(f"Optimization time for TDM: {tot_time_TD:.2f} seconds")
 
     # SDM
     start_time_SD = time.time()
-    optimized_control_points_SD, tot_iter_SD, avg_error_SD = gradient_descent_SD(P, T, knots, degree, X, max_iter=max_iter, constraint=True)
+    optimized_control_points_SD, tot_iter_SD, avg_error_SD = gradient_descent_SD(P, T, knots, degree, X, max_iter=max_iter, constraint=constraint)
     end_time_SD = time.time()
     tot_time_SD = end_time_SD - start_time_SD
     print(f"Optimization time for SDM: {tot_time_SD:.2f} seconds")
@@ -61,12 +61,7 @@ if __name__ == "__main__":
     #X = np.array([[0.0, 0.0],[0.5, 0.5], [1.0, 0.0]])
     X = generate_sinus_cloud_points(50, 0.02)
     #visualize_points(X)
-    Pc = np.array([
-        [0.0, 0.0],
-        [0.1, 0.0],
-        [0.4, 0.0],
-        [1.0, 0.0]
-    ])
+    Pc = np.array([[0.0, 0.0],[0.1, 0.0],[0.4, 0.0],[1.0, 0.0]])
     degree = 2
     n = len(Pc)
     # knot size = nb_ctrlpts + degree + 1
@@ -188,7 +183,6 @@ if __name__ == "__main__":
 
     X = generate_square_cloud_points(200, 0)
     #visualize_points(X)
-    #Pc = np.array([ [0.0, 0.0], [-0.5, 1.5], [0.5, 1.0], [1.5, 1.5], [1.0, 0.0]])
     Pc = np.array([ [0.0, 0.0], [0.0, 0.8],[0.20, 1.0],[0.8, 1.0], [1.0, 0.8], [1.0, 0.0]])
     degree = 2
     n = len(Pc)
@@ -212,7 +206,7 @@ if __name__ == "__main__":
     visualize_data_curve_footpoints_controlpoints(X, unoptimized_curve, footpoints_on_the_curve, Pc)
     
     print("Simulate Square shape \n")
-    results = compare3methods(Pc,T,X, max_iter=500)
+    results = compare3methods(Pc,T,X, max_iter=100)
 
     #optimized_control_points_SD = results["control_points"][2]
     #visualize_data_curve_controlpoints(X, np.array([bspline_curve(optimized_control_points_SD, t, knots, degree) for t in t_vals]), optimized_control_points_SD)
@@ -230,7 +224,7 @@ if __name__ == "__main__":
     X = generate_naca0012airfoil(100)
     #visualize_points(X)
     #Pc = np.linspace(0,1,5)[:, np.newaxis] ; Pc = np.hstack((Pc, np.zeros_like(Pc)))
-    Pc = np.array([[0.0, 0.0], [0.0, 0.05], [0.7, 0.05], [1.0, 0.0]])
+    Pc = np.array([[0.0, 0.0],[0.0, 0.05],[0.7, 0.05],[1.0, 0.0]])
     degree = 2
     n = len(Pc)
     # knot size = nb_ctrlpts + degree + 1
@@ -313,7 +307,7 @@ if __name__ == "__main__":
     X = generate_curve2(100)
     #visualize_points(X)
     #Pc = np.array([[-0.1, 0.0], [0, 1/30], [0.1, 2/30], [0.2, 0.1]])
-    Pc = np.array([[-0.1, 0.0], [-0.1, 0.05], [-0.075, 0.075], [0.0, 0.1], [0.2, 0.1]])
+    Pc = np.array([[-0.1, 0.0],[-0.1, 0.05],[-0.075, 0.075],[0.0, 0.1],[0.2, 0.1]])
     degree = 2
     n = len(Pc)
     # knot size = nb_ctrlpts + degree + 1
@@ -336,7 +330,7 @@ if __name__ == "__main__":
     visualize_data_curve_footpoints_controlpoints(X, unoptimized_curve, footpoints_on_the_curve, Pc)
     
     print("Simulate Curve 2 \n")
-    results = compare3methods(Pc,T,X, max_iter=500)
+    results = compare3methods(Pc,T,X, max_iter=100)
 
     #optimized_control_points_SD = results["control_points"][2]
     #visualize_data_curve_controlpoints(X, np.array([bspline_curve(optimized_control_points_SD, t, knots, degree) for t in t_vals]), optimized_control_points_SD)
@@ -353,14 +347,7 @@ if __name__ == "__main__":
 
     X = generate_circle_cloud_points(100, 0)
     #visualize_points(X)
-    Pc = np.array([
-        [  0.0, -1.0],
-        [ -1.0, -1.0],
-        [ -1.0,  1.0],
-        [  1.0,  1.0],
-        [  1.0, -1.0],
-        [  0.0, -1.0],
-    ])
+    Pc = np.array([[0.0, -1.0],[-1.0, -1.0],[-1.0,  1.0],[1.0,  1.0],[1.0, -1.0],[0.0, -1.0]])
     degree = 2
     n = len(Pc)
     # knot size = nb_ctrlpts + degree + 1
@@ -383,7 +370,7 @@ if __name__ == "__main__":
     visualize_data_curve_footpoints_controlpoints(X, unoptimized_curve, footpoints_on_the_curve, Pc)
     
     print("Simulate Circle shape \n")
-    results = compare3methods(Pc,T,X, max_iter=10)
+    results = compare3methods(Pc,T,X, max_iter=50)
 
     #optimized_control_points_SD = results["control_points"][2]
     #visualize_data_curve_controlpoints(X, np.array([bspline_curve(optimized_control_points_SD, t, knots, degree) for t in t_vals]), optimized_control_points_SD)
@@ -400,16 +387,10 @@ if __name__ == "__main__":
     X = cloud_points_reader("double_ellipsoid.txt")
     #visualize_points(X)
     
-    Pc = np.array([
-        [ 0.016,  -0.015],
-        [ -0.10,   0.000],
-        [ -0.03,   0.013],
-        [-0.025,   0.025],
-        [ 0.016,   0.025],
-        [ 0.016,   0.005],
-        [ 0.016,  -0.015]
-    ])
-    degree = 3
+    Pc = np.array([[-0.03, 0.013],[-0.025, 0.025],[0.0, 0.025],[0.01, 0.025],[0.016, 0.02],[0.016, -0.012],[0.013, -0.015],[0.0, -0.015],[-0.10, -0.002],[-0.03, 0.013]])
+    
+
+    degree = 2
     # knot size = nb_ctrlpts + degree + 1
     n = len(Pc)
     knots = np.arange(n - degree + 1) / (n -degree)
@@ -432,7 +413,47 @@ if __name__ == "__main__":
     visualize_data_curve_footpoints_controlpoints(X, unoptimized_curve, footpoints_on_the_curve, Pc)
     
     print("Simulate Double Ellipsoid \n")
-    results = compare3methods(Pc,T,X, max_iter=500)
+    results = compare3methods(Pc,T,X, max_iter=50)
+
+    #optimized_control_points_SD = results["control_points"][2]
+    #visualize_data_curve_controlpoints(X, np.array([bspline_curve(optimized_control_points_SD, t, knots, degree) for t in t_vals]), optimized_control_points_SD)
+    """
+
+    #################################################
+    #                                               #
+    #                      Apollo                   #
+    #                                               #
+    #################################################
+
+    """
+    X = cloud_points_reader("apollo.txt")
+    #visualize_points(X)
+
+    Pc = np.array([[13, 65],[17.5,76],[25,65],[115,8],[130,0],[115,-8],[28,-65],[15,-75],[12,-65],[-10,0.0],[13, 65]])
+
+    degree = 3
+    # knot size = nb_ctrlpts + degree + 1
+    n = len(Pc)
+    knots = np.arange(n - degree + 1) / (n -degree)
+    knots = np.concatenate(([knots[0]]*degree, knots, [knots[-1]]*degree))
+
+    # OPTIMIZATION
+
+    # first foot points calculus
+    T = all_tk(X, Pc, knots, degree)
+    footpoints_on_the_curve = np.array([
+        bspline_curve(Pc, t, knots, degree) for t in T
+    ])
+
+    t_vals = np.linspace(0, 1, 100)
+    unoptimized_curve = np.array([bspline_curve(Pc, t, knots, degree) for t in t_vals])
+    #plt.title(f"Initial Bspline curve before optimization \n degree= ${degree}$ and ${len(X)}$ data points")
+    #visualize_data_curve_controlpoints(X, unoptimized_curve, Pc)
+    plt.title(f"Bspline curve with foot points before optimization \n degree= ${degree}$ and ${len(X)}$ data points")
+    visualize_data_curve_footpoints_controlpoints(X, unoptimized_curve, footpoints_on_the_curve, Pc)
+    
+    print("Simulate Apollo \n")
+    results = compare3methods(Pc,T,X, max_iter=10, constraint=True)
 
     #optimized_control_points_SD = results["control_points"][2]
     #visualize_data_curve_controlpoints(X, np.array([bspline_curve(optimized_control_points_SD, t, knots, degree) for t in t_vals]), optimized_control_points_SD)
